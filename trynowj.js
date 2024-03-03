32ed13837730c6dbd8e61a8909043b44787731f7
Dhyaneshwari Bhende <110776460+DhyaneshwariBhende@users.noreply.github.com>
2024-03-03 17:28:00 +0530

    Add files via upload

---
+++ trynowj.js
@@ -0,0 +1,9 @@
+/*document.addEventListener("DOMContentLoaded", function () {
+    var closeButton = document.getElementById("close-popup-button");
+
+    // Function to handle button click event
+    closeButton.addEventListener("click", function () {
+        // Close the popup window
+        window.close();
+    });
+});*/
